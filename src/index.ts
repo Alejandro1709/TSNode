623d07613aabcd9857137a72777405b3c6e418d7
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-04-07 09:29:03 -0500

    connect route to be accessible

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import diariesRoutes from './routes/diaries';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+
+app.use('/api/v1/diaries', diariesRoutes);
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello');
